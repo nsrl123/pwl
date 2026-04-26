94c3be69153b63436c47b70e180eabd2ec00506a
Nasrul Ilmi <nsrl123@users.noreply.github.com>
2026-04-26 19:26:31 +0700

    Menyelesaikan tugas Capstone 5 CI4 - Filter

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,12 +5,12 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
+
+$routes->get('/', 'Home::index', ['filter' => 'auth']);
 
 $routes->get('login', 'AuthController::login');
 $routes->post('login', 'AuthController::login');
 $routes->get('logout', 'AuthController::logout');
 
-
-$routes->get('/produk', 'ProdukController::index');
-$routes->get('/keranjang', 'TraksaksiController::index');
+$routes->get('produk', 'ProdukController::index', ['filter' => 'auth']);
+$routes->get('keranjang', 'TraksaksiController::index', ['filter' => 'auth']);
